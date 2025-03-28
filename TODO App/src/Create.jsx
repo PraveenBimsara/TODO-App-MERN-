7b69005f07f3c6ec0b7e66f2cc4296b2e90c8357
PraveenBimsara <praveenbimsara99@gmail.com>
2025-03-29 01:36:47 +0530

    backend setup

--- TODO App/src/Create.jsx
+++ TODO App/src/Create.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Create() {
+    const [task, setTask] = useState()
+
+    const handleAdd = () => {}
   return (
     <div className='create_form'>
         <input type="text" name="" id="" placeholder='Enter Task'/>
-        <button type="button">Add</button>
+        <button type="button" onClick={handleAdd}>Add</button>
     </div>
   )
 }
