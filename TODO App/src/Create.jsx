56a4afe6b28179939fe57316e97668c1065534e1
PraveenBimsara <praveenbimsara99@gmail.com>
2025-03-29 14:17:36 +0530

    mongodb connect

--- TODO App/src/Create.jsx
+++ TODO App/src/Create.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function Create() {
     const [task, setTask] = useState()
 
-    const handleAdd = () => {}
+    const handleAdd = () => {
+      axios.post('http://localhost:3001/add', {task: task})
+      .then(result => console.log(result))
+      .catch(err => console.log(err))
+    }
   return (
     <div className='create_form'>
         <input type="text" name="" id="" placeholder='Enter Task'/>
--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const TodoModel = require('./models/Todo')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect('mongodb://praveen:praveen@cluster0-shard-00-00.psiblyz.mongodb.net:27017/todo?authSource=admin')
+
+app.post('/add', (req,res) => {
+    const task = req.body.task;
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
 
 app.listen(3001, () => {
     console.log("Server is running")
